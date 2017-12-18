358fc88bb29b77e78ca97541d3234334da4be2aa
ppibburr <tulnor33@gmail.com>
2017-12-18 05:30:36 -0500

    ...

--- ema.rb
+++ ema.rb
@@ -1,67 +1,106 @@
 load "./bin/repl"
 
-def signal a,b
-  return 0 if (a <=> b) == 0
-  return -1 if a < b*0.995
-  return  1 if a > b*1.005
-end
+class << self
+  def signal a,b
+    return 0 if (a <=> b) == 0
+    return -1 if a < b*0.995
+    return  1 if a > b*1.005
+  end
 
-def order
-  $c+=1
-end
+  def order
+    @c+=1
+  end
 
-def buy r
-  order
-  $amt = ($base / r)*0.9975
-  $hold = true
-end
+  def buy r
+    order
+    @amt = (@base / r)*0.9975
+    @hold = true
+  end
 
-def buy r
-  order
-  $base = ($amt * r)*0.9975
-  $hold = false
-end
+  def buy r
+    order
+    @base = (@amt * r)*0.9975
+    @hold = false
+  end
 
-$base = 1000
-$amt  = 0
-$c    = 0
-
-Client.repl self
-
-connect
-
-c.updates do |obj|
-  p obj['err']
-  next if obj['err']
-  tick = obj['result']
-  p tick['market']
-  next unless tick and tick['market'].upcase == "BTC-AEON"
-
-  puts `clear`
+  attr_reader :ema, :tick, :price
+  def rate
+    if @sim
+      return price
+    end
   
-  ema   = tick['ema12']
-  price = tick['last']
-  
-  s = signal(price, ema)
-  
-  $s ||= s
-  $sc = (s != $s)
-  
-  $init = true if $sc or $init
-  
-  if init
-    if s == -1 and $hold
-      buy tick['market_order']['bid']['rate']
-    elsif s == 1 and $hold
-      sell tick['market_order']['ask']['rate']
+    if @hold 
+      tick['market_order']['bid']['rate']
+    else
+      tick['market_order']['ask']['rate']
     end
   end
   
-  puts JSON.pretty_generate(tick)
-  puts tick['market'].upcase+" Last: #{tick['last']}"
-  puts "##{$c} #{$amt}COIN #{$base}BASE"
+  def step
+    @offset ||= -1
+    @offset += 1
+    @ema   = @history['ema'][@offset] 
+    @price = @history['rates'][@offset]
+  end
+
+  def run
+    @base = 1000
+    @amt  = 0
+    @c    = 0
+
+    Client.repl self
+
+    connect
+
+    c.updates do |obj|
+      @tick = nil
+      next if obj['err']
+      @tick = obj['result']
+      next unless tick and tick['market'].upcase == "USDT-BTC"
+
+      puts `clear`
+      
+      @ema    = tick['ema12']
+      @price  = tick['last']
+      
+      next unless ema and price
+      
+      analyze
+    end
+
+    while true
+      if @sim = ARGV.index("-s")
+        c.history ARGV[0], 90 do |o|
+          if @history = o['result']
+            while step
+              analyze
+            end
+          end
+          break
+        end
+      end
+      Thread.pass
+    end
+  end
+  
+  def analyze
+    s = signal(rate, ema)
+    
+    @s ||= s
+    @sc = (s != @s)
+    
+    @init = true if @sc or @init
+    
+    if @init
+      if s == -1 and @hold
+        buy  rate
+      elsif s == 1 and @hold
+        sell rate
+      end
+    end
+    
+    print "\rLast: #{rate} ##{@c} #{@amt}COIN #{@base}BASE"  
+  end
 end
 
-while true
-  Thread.pass
-end
+run
