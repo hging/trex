2d471256b9b2a797c303e77d32999a4cb1ff5ea1
ppibburr <tulnor33@gmail.com>
2017-12-24 14:02:12 -0500

    ...

--- rapid.rb
+++ rapid.rb
@@ -8,11 +8,13 @@ def rapid
     if last < ema
       return :buy
     end   
+    
   elsif @hold
-    if (usd - @last_buy) > 5#+@loss
+    if (profit / @last_buy) > 1.01#+@loss
       @loss = 0
       return :sell
-    elsif (l=(@last_buy - usd)) > 100
+    
+    elsif (l=(@last_buy - profit)) > 100
       @loss+=l
       return :sell
     end
@@ -21,24 +23,49 @@ def rapid
   :hold
 end
 
+def profit
+  (@lam*last*0.9975)
+end
+
+def order type
+
+end
+
 def buy
   @hold = true
-  @last_buy = base
-  @amount   = ((base) / last)*0.995
-  @sb ||= amount
-  @base = 0
+  
+  if sim_order?
+    @last_buy = base*1.0
+    @amount   += (@lam=((base*1.0) / last)*0.9975)
+    @base = base*0.0
+  else
+    order :buy
+  end
+  
+  @sb ||= ((base) / last)*0.9975
+
+  @buys+=1
+  puts
   p [:buy, last]
 end
 
 def sell
   @hold   = false
-  @base   = usd
-  @amount = 0
+  
+  if sim_order?
+    @base   += (usd*1.0)
+    @amount = @amount*0.0
+  else
+    order :sell
+  end
+  
+  @sells+=1
+  puts
   p [:sell, last]
 end
 
 def usd
-  amount*last*0.995
+  amount*last*0.9975
 end
 
 def hold
@@ -47,6 +74,10 @@ end
 
 class << self
   attr_accessor :ema, :ema_periods, :history_periods, :history, :last, :close, :candle, :market, :base, :amount, :last_buy
+  def sim_order?
+    @simulate or @sim_orders
+  end
+  
   def simulate
     @simulate = true
    
@@ -70,6 +101,9 @@ class << self
     end
     
     history.each_with_index do |r, i|
+      (@np ||=0)
+       @np += 1
+      
       @ema = @sim_ema[i]
       
       @last_tick = HashObject.becomes({
@@ -84,6 +118,7 @@ class << self
   
   def on_candle candle, offset: 0
     @np||=0
+    
     c.get_ema(market, ema_periods) do |ema, err|
       puts "periods: #{@np+=1}"
       if !err
@@ -110,21 +145,39 @@ class << self
   def report
     return unless last_tick
     print `clear`
+    puts "["
     pp last_tick
+    puts ","
     return unless last and ema
     
     @sb ||= base/last
     
-    puts "periods: #{@np}"
-    puts "#{"%.8f" % @ema} : #{"%.8f" % @last} : #{"%.8f" % b=base+(amount*last)}"
-    p [s=(@sb)*last, e=(amount*last)+base, e / s]
+    puts JSON.pretty_generate({
+      buys:       @buys,
+      sells:      @sells,
+      hodl:       s=(@sb)*last, 
+      base_value: e=(amount*last)+base,
+      coin:       "%.8f" % amount, 
+      base:       "%.8f" % base,
+      pct:        e / s,
+      periods:    @np,
+      bid:        last_tick.bid,
+      ask:        last_tick.last,
+      last:       last,
+      ema:        ema
+    }, allow_nan: true)
+    puts "]"
   end
   
   def run
     connect
+  
+    @buys = @sells = 0
 
     @history_periods     = (ARGV[1] ||= 60).to_i
     @ema_periods         = (ARGV[2] ||= 12).to_i
+
+    @sim_orders = (ARGV.index("-s") or ARGV.index("-S"))
 
     c.subscribe @market=ARGV[0] do |o,e|
       raise "#{e}" if e
@@ -140,16 +193,19 @@ class << self
           else
             store.get_ema :ema, market, ema_periods
           
-            @amount = 0
-            @base   = 100
+            c.balances market: market do |b, e|
+              @amount = b.coin['avail']
+              @base   = b.base['avail']
           
-            candles
+              candles
             
-            c.updates do |tick, err|
-              if tick.market == market
-                @last_tick = tick
-                @last      = tick.last
-                report
+              c.updates do |tick, err|
+                if tick.market == market
+                  @last_tick = tick
+                  @last      = tick.last
+                  
+                  report
+                end
               end
             end
           end
